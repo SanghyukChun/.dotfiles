75a489f8a963abac37691fe433296a580f087adb
SanghyukChun <sanghyuk.chun@gmail.com>
2020-06-27 15:58:43 +0900

    fix typo

--- zsh_config.sh
+++ zsh_config.sh
@@ -2,7 +2,7 @@
 
 echo "installing oh-my-zsh..."
 if [ ! -f "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     mv ~/.zshrc ~/.zshrc.old
     cp ./zshrc ~/.zshrc
 else
